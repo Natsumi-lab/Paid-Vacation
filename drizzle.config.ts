ec90544fda0c613ff19d47fd637f5ed55ebf0cb9
Natsumi-lab <imustan81@gmail.com>
2026-04-17 21:47:03 +0900

    drizzle tursoからsqliteに変更

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,9 +3,8 @@ import type { Config } from "drizzle-kit";
 export default {
   schema: "./src/lib/db/schema.ts",
   out: "./drizzle",
-  dialect: "turso",
+  dialect: "sqlite",
   dbCredentials: {
-    url: process.env.DATABASE_URL || "file:local.db",
-    authToken: process.env.DATABASE_AUTH_TOKEN,
+    url: "local.db",
   },
 } satisfies Config;
